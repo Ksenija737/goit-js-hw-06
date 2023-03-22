ab0544e1fadfff6414d8b4f5a67a92626c0a43e8
Ksenija737 <117684062+Ksenija737@users.noreply.github.com>
2023-03-22 16:40:12 +0100

    1 task

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const listWithId = document.querySelectorAll('#categories li.item');
+const countId = listWithId.length;
+console.log(`Number of categories: ${countId}`);
+
+listWithId.forEach((item) => {
+    console.log(`Category: ${item.querySelector('h2').textContent} `);
+    console.log(`Elements: ${item.querySelectorAll('li').length }`);
+});
---
+++ src/js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const container = document.querySelector(#ingredients);
