a79bfa76fa8742939c929b52de5ff34109ec06ac
Ksenija737 <117684062+Ksenija737@users.noreply.github.com>
2023-03-31 21:33:48 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,8 @@
 const form = document.querySelector('.login-form');
+form.addEventListener("submit", onSubmit);
+
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.dir(event.currentTarget.elements);
+}
